fdacf3ff1a3a0ed8c957db4742be339781c50896
dzmitrybutar <verysweetleaf@gmail.com>
2019-10-13 20:02:13 +0300

    task_2 init

---
+++ task2/main.py
@@ -0,0 +1,35 @@
+import re
+
+
+class Version:
+    def __init__(self, version):
+        self.__call__(version)
+
+    expression = re.compile(
+        r'^(\d+)\.(\d+)(\.(\d+))?[-.]?([a-z]+(\d+)?)?[-.]?([a-z]+(\d+)?)?$')
+
+    def __call__(self, version):
+        result = self.expression.match(version)
+
+        if not result:
+            raise ValueError("Invalid version '%s'" % version)
+
+
+def main():
+    to_test = [
+        ('1.0.0', '2.0.0'),
+        ('1.0.0', '1.42.0'),
+        ('1.2.0', '1.2.42'),
+        ('1.1.0-alpha', '1.1.0-beta'),
+        ('1.0.1b', '1.0.10-alpha.beta'),
+        ('1.0.0-rc.1', '1.0.0'),
+    ]
+
+    for version_1, version_2 in to_test:
+        assert Version(version_1) < Version(version_2), 'le failed'
+        assert Version(version_2) > Version(version_1), 'ge failed'
+        assert Version(version_2) != Version(version_1), 'neq failed'
+
+
+if __name__ == "__main__":
+    main()
