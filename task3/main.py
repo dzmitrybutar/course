3101bc66323e70ddbbcd34131dcee013cd989e2d
dzmitrybutar <verysweetleaf@gmail.com>
2019-10-14 13:05:34 +0300

    task_3 use TreadPoolExecutor

--- task3/main.py
+++ task3/main.py
@@ -1,27 +1,20 @@
-from threading import Thread
-
-a = 0
+import concurrent.futures
 
 
 def function(arg):
     b = 0
     for _ in range(arg):
         b += 1
-
-    global a
-    a += b
+    return b
 
 
 def main():
-    threads = []
-    for i in range(5):
-        thread = Thread(target=function, args=(1000000,))
-        thread.start()
-        threads.append(thread)
-
-    [t.join() for t in threads]
+    a = 0
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = [executor.submit(function, 10000000) for _ in range(5)]
+        for c in concurrent.futures.as_completed(results):
+            a += c.result()
     print("----------------------", a)
 
 
-if __name__ == "__main__":
-    main()
+main()
